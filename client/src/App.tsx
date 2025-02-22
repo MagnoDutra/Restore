274ff8edf71318b21d7c0d08d20c5021c1d0d69e
Magno <jmagno.dutra@hotmail.com>
2025-02-22 14:30:53 -0300

    criado requisicao para api

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [products, setProducts] = useState([
-    { name: "product1", price: 100.0 },
-    { name: "product2", price: 200.0 },
-  ]);
+  const [products, setProducts] = useState<{ name: string; price: number }[]>(
+    []
+  );
+
+  useEffect(() => {
+    fetch("https://localhost:5000/api/products")
+      .then((res) => res.json())
+      .then((data) => setProducts(data));
+  }, []);
 
   const addProduct = () => {
     setProducts((prevState) => [
